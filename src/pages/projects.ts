3f8a6fafd8f9af5eef3cb00ef215363dbdb0fb3b
Alexis Cheron <alexisnorech@gmail.com>
2020-12-28 01:03:06 +0400

    fixed projects tab and reduced amount of requests

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -17,7 +17,6 @@ export async function projects(session: Session, state: State)
 {
     const { data } = await loader(async () => {
         const dashboard = await getDashboard(session);
-        let promises = [];
         let projets = dashboard.data.board.projets;
 
         for (const i in projets) {
@@ -27,39 +26,28 @@ export async function projects(session: Session, state: State)
                 continue;
             }
             projet.is_hidden = false;
-            promises.push((async () => {
-                const { data } = await get(session, projet.title_link);
-                if (data.register == "1")
-                    projet.not_registered = true;
-                projet.is_note = data.is_note;
-                projet.is_projet = data.is_projet;
-                projet.activity = data;
-            })());
         }
-        await Promise.all(promises);
         return (dashboard);
     });
-    const projects = data.board.projets.filter(p => p.is_projet);
+    const projects = data.board.projets;
     const visibleProjects = projects.filter(p => !p.is_hidden);
 
     term.table([
-        [ 'Projet ', 'Inscrit ', 'Fin d\'inscription ', 'Début ', 'Fin ', 'Timeline ' ],
+        [ 'Projet ', 'Inscrit ', 'Début ', 'Fin ', 'Timeline ' ],
         ...visibleProjects.map(projet => {
-            const note_state = projet.is_note ? " ^b⏺": "";
             const timeline_percent = parseFloat(projet.timeline_barre);
-            const register_state = projet.not_registered ? "^rNON INSCRIT" : "^gINSCRIT";
-            let date_inscription = projet.date_inscription || "Fermé";
+            let register_state = projet.date_inscription == false ? "^gINSCRIT" : "^rNON INSCRIT ^w(" + projet.date_inscription + ")";
             let timeline_start = projet.timeline_start;
             let timeline_end = projet.timeline_end;
-            let timeline = Math.round(timeline_percent * 100) / 100 + "%";
+            let timeline = (Math.round(timeline_percent * 100) / 100).toFixed(2) + "%";
             if (term.width <= 100) {
-                date_inscription = date_inscription.split(',')[0];
+                register_state = register_state.split(' ^w(')[0];
                 timeline_start = timeline_start.split(',')[0];
                 timeline_end = timeline_end.split(',')[0];
             }
             if (term.width > 100)
                 timeline = staticProgress(timeline_percent, 20) + " " + timeline;
-            return [ projet.title + note_state + " ", register_state + " ", date_inscription + " ", timeline_start + " ", timeline_end + " ", timeline ]
+            return [ projet.title + " ", register_state + " ", timeline_start + " ", timeline_end + " ", timeline ]
         })
     ], {
         hasBorder: false,
@@ -104,11 +92,8 @@ export async function projects(session: Session, state: State)
         }
         const project = projects.find(p => p.title == input);
 
-        if (typeof project.activity === "undefined") {
-            const { data } = await loader(() => get(session, project.title_link));
-            project.activity = data;
-        }
+        const { data: activity } = await loader(() => get(session, project.title_link));
 
-        return loadPage(displayProject, session, state, project.title_link + "project/", project.activity);
+        return loadPage(displayProject, session, state, project.title_link + "project/", activity);
     };
 }
