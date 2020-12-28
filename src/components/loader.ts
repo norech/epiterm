a0eec75e4d78ad4db92d482b1eeae7b2f27a0673
Alexis Cheron <alexisnorech@gmail.com>
2020-12-28 14:42:39 +0400

    fixed spinner after error in loader

--- src/components/loader.ts
+++ src/components/loader.ts
@@ -3,10 +3,18 @@ import { terminal as term } from  'terminal-kit';
 export async function loader<T>(action: () => Promise<T>, message?: string): Promise<T> {
     term.clear();
     const spinner = await term.spinner('unboxing-color');
+    let data;
     term( message || ' Chargement... ' );
-    const data = await action();
-    spinner.animate(false);
-    term.clear();
-    term.moveTo(1, 2);
+    try {
+        data = await action();
+        spinner.animate(false);
+        term.clear();
+        term.moveTo(1, 2);
+    } catch(ex) {
+        spinner.animate(false);
+        term.clear();
+        term.moveTo(1, 2);
+        throw ex;
+    }
     return data;
 }
